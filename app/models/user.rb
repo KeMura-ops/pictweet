324968ba91220435ae5dd410b50eaebe59e03380
KeMura-ops <kenti432@yahoo.co.jp>
2021-04-26 16:24:27 +0900

    各モデルにアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :comments
-
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+  
   validates :nickname, presence: true, length: { maximum: 6 }
 end
