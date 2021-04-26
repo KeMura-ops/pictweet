c0beb7f76540843eb3eb17b6133ff74f06d38fde
KeMura-ops <kenti432@yahoo.co.jp>
2021-04-26 16:43:53 +0900

    コメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :comments
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_of_relationships, source: :user
+  has_many :relationships # データを取得するための入り口(フォロー)
+  has_many :followings, through: :relationships, source: :follow # データを取得後の出口(フォロー)
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id' # 入り口(フォロワー)
+  has_many :followers, through: :reverse_of_relationships, source: :user # 出口(フォロワー)
   
   validates :nickname, presence: true, length: { maximum: 6 }
 end
